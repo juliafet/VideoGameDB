08ec73e4fe6c37f353206f95c9c091a5654985c8
Julia Fetisova <j.s.fetisova@gmail.com>
2022-06-14 09:48:50 +1000

    enterceptors added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
+import { HttpService } from './../../services/http.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { APIResponse, Game } from 'src/app/models';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public sort!: string;
+  public games!: Array<Game>
 
-  constructor() { }
+  constructor(
+    private HttpService: HttpService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+        if (params['game-search']) {
+            this.searchGames('metacrit', params['game-search'])
+        } else {
+            this.searchGames('metacrit')
+        }
+    })
+  }
+
+  searchGames(sort: string, search?: string): void {
+    this.HttpService.getGameList(sort, search)
+    .subscribe((gameList: APIResponse<Game>) => {
+        this.games = gameList.results;
+        console.log(gameList)
+    })
   }
 
 }
